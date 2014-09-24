864ed85fc2f72b803f281fe5a83b23a95c91e13e
tssingkofer <tssingkofer@users.noreply.github.com>
2014-09-24 14:24:17 -0500

    Create csvToSQL.sh
    inital commit. Only sample script, framed against sample data.

---
+++ csvToSQL.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+OLDIFS=$IFS
+
+curl -L "https://docs.google.com/spreadsheets/export?id=1ruAmiMONHHg3VqpmuTL6p3L0Om-qS1nowBkxMx_nE3w&exportFormat=csv&gid=0" -o ./TestSheet
+
+echo
+echo
+
+IFS=","
+while read f1 f2 f3
+do
+      echo "insert into user (name, birthdate, sex) VALUES($f1,$f2,$f3);"
+done < TestSheet
+IFS=$OLDIFS
