7a4f048357a1492900b7d63db6c7739dfaccfa75
tssingkofer <tssingkofer@users.noreply.github.com>
2014-09-25 12:03:55 -0500

    Using new sheet, actual data, and new table.

--- csvToSQL.sh
+++ csvToSQL.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 OLDIFS=$IFS
 
-curl -L "https://docs.google.com/spreadsheets/export?id=1ruAmiMONHHg3VqpmuTL6p3L0Om-qS1nowBkxMx_nE3w&exportFormat=csv&gid=0" -o ./TestSheet
+curl -L "https://docs.google.com/spreadsheets/export?id=1sxIMF7H_ksLk34iFSgO8JFCbSHABlE_BPk3NNsdBOGo&exportFormat=csv&gid=0" -o ./DashSheet
 
 echo
 echo
 echo "use dash;" > ./insertSQL.sql
 
 IFS=","
-while read f1 f2 f3
+while read f1 f2 f3 f4 f5 f6 f7 f8 f9 f10 f11 f12 f13 f14 f15 f16 f17 f18 f19 f20
 do
-      echo "insert into test (name, birthdate, sex) SELECT * FROM (SELECT '$f1','$f2','$f3') as tmp WHERE (Select count(*) from test where name='$f1') = '0';" >> ./insertSQL.sql
-done < TestSheet
+      echo "insert into trips (trip_date, start_address, start_weather, start_latitude, start_longitude, start_map, trip_score, trip_distance, engine_alerts, brake_alerts, accel_alerts, speed_alerts, sms_alerts, fuel_used, fuel_cost, end_address, end_weather, end_latitude, end_longitude, end_map)
+	SELECT * FROM (SELECT date(today), '$f1','$f2','$f3','$f4','$f5','$f6','$f7','$f8','$f9','$f10','$f11','$f12','$f13','$f14','$f15','$f16','$f17','$f18','$f19','$f20') as tmp;" >> ./insertSQL.sql
+done < DashSheet
 IFS=$OLDIFS
