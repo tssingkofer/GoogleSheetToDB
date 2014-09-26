55f63361f646ca8f7c93db089dc156d4409c904c
tssingkofer <tssingkofer@gmail.com>
2014-09-25 20:54:52 -0500

    Fixing obvious idiocy.

--- csvToSQL.sh
+++ csvToSQL.sh
@@ -18,7 +18,7 @@ IFS=","
 while read f1 f2 f3 f4 f5 f6 f7 f8 f9 f10 f11 f12 f13 f14 f15 f16 f17
 do
       echo "insert into trips (trip_date, start_weather, start_latitude, start_longitude, start_map, trip_score, trip_distance, engine_alerts, brake_alerts, accel_alerts, speed_alerts, sms_alerts, fuel_used, fuel_cost, end_weather, end_latitude, end_longitude, end_map)
-	SELECT * FROM (SELECT curdate() as a, '$f1' as sw,'$f2' as b,'$f3' as c,'$f4' as d,'$f5' as e,'$f6' as f,'$f7' as g,'$f8' as h,'$f9' as i,'$f10' as j,'$f11' as k,'$f12' as l,'$f13' as m,'$f14' as ew,'$f16' as n,'$f17' as o) as tmp WHERE (Select count(*) from trips where start_map='$f4') = '0';" >> ./insertSQL.sql
+	SELECT * FROM (SELECT curdate() as a, '$f1' as sw,'$f2' as b,'$f3' as c,'$f4' as d,'$f5' as e,'$f6' as f,'$f7' as g,'$f8' as h,'$f9' as i,'$f10' as j,'$f11' as k,'$f12' as l,'$f13' as m,'$f14' as ew,'$15' as n,'$f16' as o,'$f17' as p) as tmp WHERE (Select count(*) from trips where start_map='$f4') = '0';" >> ./insertSQL.sql
 done < DashSheet #Read from csv file
 
 IFS=$OLDIFS #Return to original configuration
